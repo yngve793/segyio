a4c76a05dc3c7f36be489d4fe5ff7c0e0bf5f04d
Yngve Villanger <yvi@equinor.com>
2025-04-30 10:52:34 +0200

    Introduce segy_get_field_u16
    Function used to read an uint16_t field directly from the buffer.

--- lib/include/segyio/segy.h
+++ lib/include/segyio/segy.h
@@ -52,6 +52,7 @@ int fd_set_int( field_data* fd, int val );
 
 int get_field_fd( const char* header, field_data* fd );
 int segy_get_field_u8( const char* header, int field, uint8_t* val );
+int segy_get_field_u16( const char* header, int field, uint16_t* val );
 segy_file* segy_open( const char* path, const char* mode );
 int segy_mmap( segy_file* );
 int segy_flush( segy_file* );
